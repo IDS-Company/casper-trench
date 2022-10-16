7f44e7fc9d96fea3bd87bd351941b3402feb7291
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-10-16 15:25:03 +0300

    tab-fix

--- src/lib/components/TabMenu/index.svelte
+++ src/lib/components/TabMenu/index.svelte
@@ -41,13 +41,13 @@
 	}
 
 	.option {
-		@apply px-[clamp(8px,0.6vw,0.6vw)];
+		@apply px-[clamp(8px,0.6vw,0.6vw)] max-w-max;
 		@apply cursor-pointer;
 		@apply text-[clamp(14px,1.07vw,1.07vw)] text-color-grey-footer-label text-center;
 	}
 
 	.text {
-		@apply px-[clamp(10px,2.14vw,2.14vw)] py-[clamp(8px,1.19vw,1.19vw)];
+		@apply px-[clamp(10px,2.14vw,2.14vw)] py-[clamp(8px,1.19vw,1.19vw)] max-w-max;
 	}
 
 	.selected {
