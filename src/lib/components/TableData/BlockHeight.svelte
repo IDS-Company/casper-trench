3752a3bd69bf906b481ff8df71ef6d5c75646174
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-10-16 13:06:07 +0300

    add: home for mobile

--- src/lib/components/TableData/BlockHeight.svelte
+++ src/lib/components/TableData/BlockHeight.svelte
@@ -3,11 +3,12 @@
 
 	export let blockDate: number;
 	export let blockHeight: string;
+	export let flexed = false;
 	const diff = Date.now() - blockDate;
 	const textString = timeAgo(millisToFormat(diff));
 </script>
 
-<div class="block-height">
+<div class="block-height" class:flexed>
 	<div class="block-height">
 		{blockHeight}
 	</div>
@@ -18,12 +19,16 @@
 
 <style lang="postcss">
 	.time-ago {
-		@apply text-[clamp(10px,1.07vw,1.07vw)] text-color-table-header text-opacity-50;
+		@apply text-[clamp(12px,1.07vw,1.07vw)] text-color-table-header text-opacity-50;
 		@apply mt-[clamp(2px,0.24vw,0.24vw)];
 	}
 
 	.block-height {
 		@apply leading-none;
-		@apply text-[clamp(10px,1.07vw,1.07vw)] text-color-table-header text-opacity-100;
+		@apply text-[clamp(14px,1.07vw,1.07vw)] text-color-table-header text-opacity-100;
+	}
+
+	.flexed {
+		@apply flex gap-2 items-center;
 	}
 </style>
