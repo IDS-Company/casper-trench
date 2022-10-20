28b6ea7d50f31199703caf91ba54e720c240e911
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-10-20 15:37:20 +0300

    add: sorter hover-states

--- src/lib/icons/PaginatorChevron.svelte
+++ src/lib/icons/PaginatorChevron.svelte
@@ -1,19 +1,29 @@
 <script>
 	export let color = '#8F9398';
+
+	let isHovering = false;
 </script>
 
-<svg
-	width="9"
-	height="14"
-	class="w-full h-full"
-	viewBox="0 0 9 14"
-	fill="none"
-	xmlns="http://www.w3.org/2000/svg"
->
-	<path
-		d="M7.50049 1.5L2.00049 7.5L7.50049 13"
-		stroke={color}
-		stroke-width="2"
-		stroke-linecap="round"
-	/>
-</svg>
+{#key isHovering}
+	<svg
+		width="9"
+		height="14"
+		class="w-full h-full"
+		viewBox="0 0 9 14"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		on:mouseenter={() => {
+			isHovering = true;
+		}}
+		on:mouseleave={() => {
+			isHovering = false;
+		}}
+	>
+		<path
+			d="M7.50049 1.5L2.00049 7.5L7.50049 13"
+			stroke={isHovering ? '#099B91' : color}
+			stroke-width="2"
+			stroke-linecap="round"
+		/>
+	</svg>
+{/key}
