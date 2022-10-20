fa2a8d6ae0cd6055e3513e93d404c158ca77d573
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-10-20 15:45:29 +0300

    fix table sorter hover

--- src/lib/icons/PaginatorChevron.svelte
+++ src/lib/icons/PaginatorChevron.svelte
@@ -4,7 +4,7 @@
 	let isHovering = false;
 </script>
 
-{#key isHovering}
+{#key color}
 	<svg
 		width="9"
 		height="14"
@@ -12,12 +12,14 @@
 		viewBox="0 0 9 14"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
-		on:mouseenter={() => {
+		on:mouseover={() => {
 			isHovering = true;
 		}}
-		on:mouseleave={() => {
+		on:focus
+		on:mouseout={() => {
 			isHovering = false;
 		}}
+		on:blur
 	>
 		<path
 			d="M7.50049 1.5L2.00049 7.5L7.50049 13"
