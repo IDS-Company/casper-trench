a5c8c986e2b7678beac87240ffef3f31fd2ebf48
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-10-20 15:47:11 +0300

    cleanup

--- src/lib/icons/PaginatorChevron.svelte
+++ src/lib/icons/PaginatorChevron.svelte
@@ -4,28 +4,26 @@
 	let isHovering = false;
 </script>
 
-{#key color}
-	<svg
-		width="9"
-		height="14"
-		class="w-full h-full"
-		viewBox="0 0 9 14"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-		on:mouseover={() => {
-			isHovering = true;
-		}}
-		on:focus
-		on:mouseout={() => {
-			isHovering = false;
-		}}
-		on:blur
-	>
-		<path
-			d="M7.50049 1.5L2.00049 7.5L7.50049 13"
-			stroke={isHovering ? '#099B91' : color}
-			stroke-width="2"
-			stroke-linecap="round"
-		/>
-	</svg>
-{/key}
+<svg
+	width="9"
+	height="14"
+	class="w-full h-full"
+	viewBox="0 0 9 14"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	on:mouseover={() => {
+		isHovering = true;
+	}}
+	on:focus
+	on:mouseout={() => {
+		isHovering = false;
+	}}
+	on:blur
+>
+	<path
+		d="M7.50049 1.5L2.00049 7.5L7.50049 13"
+		stroke={isHovering ? '#099B91' : color}
+		stroke-width="2"
+		stroke-linecap="round"
+	/>
+</svg>
